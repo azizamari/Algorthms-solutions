4fe2ff7d756920bf4d8ca2c0c3b7ad0d26fef4b5
azizamari <azizamari2003@gmail.com>
2021-07-08 20:06:28 +0100

    Add assertion and test cases for Sum of Digits

--- Solutions/Sum of Digits.py
+++ Solutions/Sum of Digits.py
@@ -1,5 +1,11 @@
 def sumOfDigits(n):
+    assert int(n)==n and n>=0, 'This needs to be an integer'
     if n==0:
         return 0
     return n%10+sumOfDigits(n//10)
-print(sumOfDigits(523))
+    
+print(sumOfDigits(153)) #=9
+print(sumOfDigits(523)) #=10
+print(sumOfDigits(111115)) #10
+print(sumOfDigits(-1)) #Assertion error
+print(sumOfDigits(18.5)) #Assertion error
