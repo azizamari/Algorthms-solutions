10767237affece1d5e1ece36ea0f41adcea11876
azizamari <azizamari2003@gmail.com>
2021-07-16 20:26:52 +0100

    Add preOrderTraversal to Binary Tree

--- Solutions/Binary Tree.py
+++ Solutions/Binary Tree.py
@@ -11,3 +11,13 @@ newBT.left.left=TreeNode(3)
 newBT.left.right=TreeNode(4)
 newBT.right.left=TreeNode(5)
 newBT.right.right=TreeNode(6)
+
+def preOrder(root):
+    if root is not None:
+        print(root.value,end='')
+        preOrder(root.left)
+        preOrder(root.right)
+    else:return
+  
+print('preOrder:')
+preOrder(newBT) 
