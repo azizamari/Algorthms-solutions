51f70d66c7ff91e9c5af11bbd08524985448c3e2
azizamari <azizamari2003@gmail.com>
2021-07-28 18:45:03 +0100

    Solve invert binary tree problem

---
+++ Solutions/Invert Tree.py
@@ -0,0 +1,30 @@
+class TreeNode:
+    def __init__(self,value):
+        self.value = value
+        self.right =None
+        self.left =None
+
+def preOrder(root):
+    if root is not None:
+        print(root.value,end='')
+        preOrder(root.left)
+        preOrder(root.right)
+    else:return
+def inverse(root):
+    if root.left is not None and root.right is not None:
+        root.left,root.right=root.right, root.left
+        inverse(root.left)
+        inverse(root.right)
+    else:return
+
+newBT=TreeNode(0)
+newBT.left=TreeNode(1)
+newBT.right=TreeNode(2)
+newBT.left.left=TreeNode(3)
+newBT.left.right=TreeNode(4)
+newBT.right.left=TreeNode(5)
+newBT.right.right=TreeNode(6)
+preOrder(newBT)
+print("\nInversing the tree...")
+inverse(newBT)
+preOrder(newBT)
