f0b806ec6b3462e31d49ee1c3e846284ab3f95d8
azizamari <azizamari2003@gmail.com>
2021-08-05 18:05:00 +0100

    Add levelorder travl for listbased binary tree

--- Solutions/BinaryTree (list).py
+++ Solutions/BinaryTree (list).py
@@ -1,34 +1,37 @@
 class BinaryTree:
     def __init__(self, size):
         self.customList=size*[None]
-        self.lastUseIndex=0
+        self.lastUsedIndex=0
         self.size=size
     def insertNode(self, value):
-        if self.lastUseIndex+1==self.size:
+        if self.lastUsedIndex+1==self.size:
             return False
-        self.lastUseIndex+=1
-        self.customList[self.lastUseIndex]=value
+        self.lastUsedIndex+=1
+        self.customList[self.lastUsedIndex]=value
         return True
     def searchNode(self, value):
         return value in self.customList
     def preOrder(self,index=1):
-        if index>self.lastUseIndex:
+        if index>self.lastUsedIndex:
             return
         print(self.customList[index],end='')
         self.preOrder(index*2)
         self.preOrder(index*2+1)
     def inOrder(self,index=1):
-        if index>self.lastUseIndex:
+        if index>self.lastUsedIndex:
             return
         self.inOrder(index*2)
         print(self.customList[index],end='')
         self.inOrder(index*2+1)
     def postOrder(self,index=1):
-        if index>self.lastUseIndex:
+        if index>self.lastUsedIndex:
             return
         self.inOrder(index*2)
         self.inOrder(index*2+1)
         print(self.customList[index],end='')
+    def levelOrder(self):
+        for i in range(1,self.lastUsedIndex+1):
+            print(self.customList[i],end='')
 
 newBT=BinaryTree(8)
 newBT.insertNode("aziz")
@@ -40,3 +43,5 @@ print()
 newBT.inOrder()
 print()
 newBT.postOrder()
+print()
+newBT.levelOrder()
